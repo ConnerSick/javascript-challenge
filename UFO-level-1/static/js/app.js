977a7be4c22464582b28b1a6b731505599787885
Conner <64811824+ConnerSick@users.noreply.github.com>
2020-08-29 17:22:18 -0700

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,50 @@
-// from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+let tableBody = d3.select('tbody');
+
+
+function buildTable(data) {
+    
+    data.forEach((dataRow) => {
+
+      let row = tableBody.append("tr");
+  
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+          cell.text(val);
+        }
+      );
+    });
+}
+    
+let filteredData = tableData;
+
+function updateTable() {
+
+  let date = d3.select("#datetime").property("value");
+  
+  let filteredData = d3.select("ufo-table");
+  // var filteredTableBody = d3.select('tbody');
+  // var Table = document.getElementById("ufo-table");
+  // Table.innerHTML = "";
+
+
+    for (let i = 0, row; row = filteredData.tr[i]; i++) {
+      if (filteredData.Date === date) {
+        filteredData[i].style.display = "initial";
+      } else {
+        filteredData[i].style.display = "none";
+      }
+    }
+
+  buildTable(filteredData);
+  console.log(filteredData[1].datetime)
+}
+
+
+d3.selectAll("#filter-btn").on("click", updateTable);
+
+
+buildTable(tableData);
+
+// console.log(tableData[1].datetime)
