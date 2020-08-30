fba991360c4b03273556113207af21ac1efb5b51
Conner <64811824+ConnerSick@users.noreply.github.com>
2020-08-29 18:07:53 -0700

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,22 +1,30 @@
-let tableData = data;
+const tableData = data;
 
-let tableBody = d3.select('tbody');
-
+// get table references
+const tbody = d3.select("tbody");
 
 function buildTable(data) {
-    
-    data.forEach((dataRow) => {
+  // First, clear out any existing data
+  tbody.html("");
 
-      let row = tableBody.append("tr");
-  
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-          cell.text(val);
-        }
-      );
-    });
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    const row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
 }
-    
+
+buildTable(tableData);
+
 let filteredData = tableData;
 
 function updateTable() {
@@ -24,18 +32,8 @@ function updateTable() {
   let date = d3.select("#datetime").property("value");
   
   let filteredData = d3.select("ufo-table");
-  // var filteredTableBody = d3.select('tbody');
-  // var Table = document.getElementById("ufo-table");
-  // Table.innerHTML = "";
 
-
-    for (let i = 0, row; row = filteredData.tr[i]; i++) {
-      if (filteredData.Date === date) {
-        filteredData[i].style.display = "initial";
-      } else {
-        filteredData[i].style.display = "none";
-      }
-    }
+  filteredData.filter(filteredData => filteredData.datetime === date);
 
   buildTable(filteredData);
   console.log(filteredData[1].datetime)
@@ -44,7 +42,3 @@ function updateTable() {
 
 d3.selectAll("#filter-btn").on("click", updateTable);
 
-
-buildTable(tableData);
-
-// console.log(tableData[1].datetime)
