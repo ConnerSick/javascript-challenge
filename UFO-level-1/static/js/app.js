83b0c151c0a9919ae4435c57b91b763121dd060c
Conner <64811824+ConnerSick@users.noreply.github.com>
2020-08-29 18:13:22 -0700

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,20 +1,15 @@
 const tableData = data;
 
-// get table references
 const tbody = d3.select("tbody");
 
 function buildTable(data) {
-  // First, clear out any existing data
+
   tbody.html("");
 
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
   data.forEach((dataRow) => {
-    // Append a row to the table body
+
     const row = tbody.append("tr");
 
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
         cell.text(val);
@@ -25,20 +20,18 @@ function buildTable(data) {
 
 buildTable(tableData);
 
-let filteredData = tableData;
+function handleClick() {
 
-function updateTable() {
+  const date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
 
-  let date = d3.select("#datetime").property("value");
-  
-  let filteredData = d3.select("ufo-table");
-
-  filteredData.filter(filteredData => filteredData.datetime === date);
+  if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
 
   buildTable(filteredData);
-  console.log(filteredData[1].datetime)
 }
 
 
-d3.selectAll("#filter-btn").on("click", updateTable);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
